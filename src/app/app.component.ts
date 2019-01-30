7deaf4f277f6aa3c328eedb59c132bf81eb56d7a
RahulGajare <42006160+RahulGajare@users.noreply.github.com>
2019-01-30 16:47:33 +0530

    Simple Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+  val: string;
+
+  expression: string="";
+
+  /**
+   *
+   */
+  constructor() {
+
+  }
+  /**
+ *  takes Values from Event(click) and concates to "expression" variable;
+ */
+  pressedNumber(value: string): void {
+    if (value === "C") {
+      this.expression = '';
+      return;
+    }
+
+
+
+    if (value === "B") {
+      this.expression = this.expression.slice(0, this.expression.length - 1);
+      return;
+    }
+
+
+    if (value === "=") {
+      var expressions = require("angular-expressions");
+      let evaluate = expressions.compile(this.expression);
+      this.expression = evaluate();
+      return;
+    }
+
+    this.expression = this.expression + value;
+    console.log(value);
+  }
 }
